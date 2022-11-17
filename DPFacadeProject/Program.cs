1d8f4c230a971dad82c0277256f5138a69c556e4
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-17 16:25:55 +0300

    Добавьте файлы проекта.

---
+++ DPFacadeProject/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DPFacadeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VisualForm form = new VisualForm(
+                new TextBoxForm(),
+                new ButtonForm(),
+                new ListForm());
+
+            form.Show();
+            Console.WriteLine();
+            form.Insert("item 1");
+        }
+    }
+}
---
+++ DPFacadeProject/VisualForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DPFacadeProject
+{
+    class TextBoxForm
+    {
+        public void Show()
+        {
+            Console.WriteLine($"Text box show");
+        }
+        public void InputText(string text)
+        {
+            Console.WriteLine($"Text box input text {text}");
+        }
+        public void ClearText()
+        {
+            Console.WriteLine($"Text box clear text");
+        }
+    }
+
+    class ButtonForm
+    {
+        public void Show()
+        {
+            Console.WriteLine($"Button show");
+        }
+        public void Active() 
+        {
+            Console.WriteLine("Button is active");
+        }
+        public void NonActive()
+        {
+            Console.WriteLine("Button is deactive");
+        }
+
+        public void Press()
+        {
+            Console.WriteLine("Button press");
+        }
+    }
+
+    class ListForm
+    {
+        public void Show()
+        {
+            Console.WriteLine($"List show");
+        }
+        public void AddItem(string text)
+        {
+            Console.WriteLine($"List box add text {text} from Text Box");
+        }
+    }
+    internal class VisualForm
+    {
+        TextBoxForm textbox;
+        ButtonForm button;
+        ListForm list;
+        public VisualForm(TextBoxForm textbox, ButtonForm button, ListForm list)
+        {
+            this.textbox = textbox;
+            this.button = button;
+            this.list = list;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Form show");
+            textbox.Show();
+            button.Show();
+            list.Show();
+            button.NonActive();
+        }
+
+        public void Insert(string item)
+        {
+            textbox.InputText(item);
+            button.Active();
+            button.Press();
+            list.AddItem(item);
+            textbox.ClearText();
+            button.NonActive();
+        }
+    }
+
+
+}
